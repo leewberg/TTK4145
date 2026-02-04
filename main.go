517362a18ef1acb4f8fe1da9e56ea30247991ed6
Jekta1 <felicic@stud.ntnu.no>
2026-02-04 18:09:19 +0100

    also forgot to update main

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
-	elevio "heislabb/source"
+	config "heislabb/source/config"
+	db "heislabb/source/database"
+	elevio "heislabb/source/elevio"
+	network "heislabb/source/network"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -48,21 +51,21 @@ func elevatorMain() {
 		panic("Need one argument, specifying the ID of this elevator")
 	}
 	var err error
-	elevio.MY_ID, err = strconv.Atoi(os.Args[1])
-	if err != nil || elevio.MY_ID < 0 || elevio.MY_ID >= elevio.NUM_ELEVATORS {
+	config.MY_ID, err = strconv.Atoi(os.Args[1])
+	if err != nil || config.MY_ID < 0 || config.MY_ID >= config.NUM_ELEVATORS {
 		panic("ID needs to be an integer between 0 and NUM_ELEVATORS-1")
 	}
 
-	elevio.Init("localhost:"+strconv.Itoa(15657+elevio.MY_ID), elevio.NUM_FLOORS)
+	elevio.Init("localhost:"+strconv.Itoa(15657+config.MY_ID), config.NUM_FLOORS)
 
 	elevio.Clear_all_lights()
-	elevio.InitOrderData()
-	elevio.InitPeers()
-	elevio.LocalElevator.Init(elevio.MY_ID)
+	db.InitOrderData()
+	db.InitPeers()
+	elevio.LocalElevator.Init(config.MY_ID)
 
 	time.Sleep(100 * time.Millisecond)
-	elevio.StartNetwork(elevio.MY_ID)
-	go elevio.Light_routine(elevio.MY_ID)
+	network.StartNetwork(config.MY_ID)
+	go elevio.Light_routine(config.MY_ID)
 	go elevio.ButtonRoutine(&elevio.LocalElevator)
 	go elevio.LocalElevator.Elev_routine()
 
